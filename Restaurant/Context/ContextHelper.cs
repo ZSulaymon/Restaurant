4a451d041c6817b3c925d6b943e669f147559898
Sulaimon Zafarov <sulaymonwork@gmail.com>
2021-11-07 18:59:12 +0500

    Add project files.

---
+++ Restaurant/Context/ContextHelper.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Restaurant.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant.Context
+{
+    public static class ContextHelper
+    {
+        public static async Task Seeding(MoviePortalContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.Roles.Where(p => p.NormalizedName.Equals("Admin")).Any())
+            {
+                var adminRole = new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                };
+
+                await roleManager.CreateAsync(adminRole);
+            }
+
+            if (!userManager.Users.Where(p => p.UserName.Equals("Admin")).Any())
+            {
+                var adminUser = new User
+                {
+                    UserName = "Admin",
+                    Email = "Admin@gmail.com"
+                };
+                var result = await userManager.CreateAsync(adminUser, "password");
+
+                if (result.Succeeded)
+                {
+                    var role = await roleManager.FindByNameAsync("Admin");
+
+                    await userManager.AddToRoleAsync(await userManager.FindByNameAsync("Admin"), role.Name);
+                }
+            }
+
+            //if (!context.MovieCategories.Any())
+            //{
+            //    var movieCategories = new List<MovieCategory>
+            //    {
+            //        new MovieCategory { Id = Guid.NewGuid(), Name = "Horror"},
+            //        new MovieCategory { Id = Guid.NewGuid(), Name = "Fantasy"},
+            //        new MovieCategory { Id = Guid.NewGuid(), Name = "Documental"},
+            //        new MovieCategory { Id = Guid.NewGuid(), Name = "Comedy"},
+            //    };
+
+            //    context.MovieCategories.AddRange(movieCategories);
+            //    await context.SaveChangesAsync();
+            //}
+        }
+    }
+}
---
+++ Restaurant/Models/Account/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant.Models.Account
+{
+    public class User : IdentityUser
+    {
+        //Create class Rest
+        //public virtual ICollection<Restaurant>  Restaurants { get; set; }
+    }
+}
